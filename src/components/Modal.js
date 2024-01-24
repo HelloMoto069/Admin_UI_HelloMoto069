4d3a647dbbaa84c721fe6fd5420a9904df6db3e5
Anuruddh Singh <hellomoto.1769@gmail.com>
2024-01-24 16:04:12 +0530

    modal update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,42 @@ import React from "react";
 const Modal = ({ editingUser, setEditingUser, handleSave, handleCancel }) => {
   return (
     <div className="modal">
-      <div className="modal-content">{/* Modal content */}</div>
+      <div className="modal-content">
+        <h3>Edit Details</h3>
+        <p>ID: {editingUser.id}</p>
+        <p>
+          Name:{" "}
+          <input
+            type="text"
+            value={editingUser.name}
+            onChange={(e) =>
+              setEditingUser({ ...editingUser, name: e.target.value })
+            }
+          />
+        </p>
+        <p>
+          Email:{" "}
+          <input
+            type="text"
+            value={editingUser.email}
+            onChange={(e) =>
+              setEditingUser({ ...editingUser, email: e.target.value })
+            }
+          />
+        </p>
+        <p>
+          Role:{" "}
+          <input
+            type="text"
+            value={editingUser.role}
+            onChange={(e) =>
+              setEditingUser({ ...editingUser, role: e.target.value })
+            }
+          />
+        </p>
+      </div>
+      <button onClick={handleSave}>Save</button>
+      <button onClick={handleCancel}>Cancel</button>
     </div>
   );
 };
